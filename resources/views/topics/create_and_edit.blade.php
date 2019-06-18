8a115054bbeac41ce761bff71c16e7cc61797520
zhoucong <zhoucong@inkanshop.com>
2019-06-18 08:43:18 +0000

    User can edit their own topic

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -38,11 +38,11 @@
                     <div class="form-group">
                     	<!-- <label for="body-field">Body</label> -->
                     	<select class="form-control" name="category_id" required>
-                            <option value="" hidden disabled selected>
+                            <option value="" hidden disabled {{ $topic->id ? '' : 'selected' }}>
                                 请选择分类
                             </option>   
                             @foreach($categories as $value)
-                            <option value="{{ $value->id }}">
+                            <option value="{{ $value->id }}" {{ $topic->category_id == $value->id ? 'selected' : '' }}>
                                 {{ $value->name }}
                             </option>
                             @endforeach
